938a0d5b80f314921985104f5254a17d1cd10c72
eljunia <angela.teneva@gmail.com>
2019-01-07 23:51:37 +0200

    Add migrations

---
+++ src/Data/Peanut.Data/PeanutContextFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Peanut.Web.Models;
+
+namespace Peanut.Data
+{
+    public class PeanutContextFactory : IDesignTimeDbContextFactory<PeanutContext>
+    {
+        public PeanutContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<PeanutContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseSqlServer(connectionString);
+
+            // Stop client query evaluation
+            builder.ConfigureWarnings(w => w.Throw(RelationalEventId.QueryClientEvaluationWarning));
+
+            return new PeanutContext(builder.Options);
+        }
+    }
+}
